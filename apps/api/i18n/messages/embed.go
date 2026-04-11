c8c454e785a1db7907c27d003c815ea057c91b5a
Walter Santacruz <wasantac12349@gmail.com>
2026-04-10 22:04:47 -0500

    docs: add doc comment to embed.FS explaining purpose

--- apps/api/i18n/messages/embed.go
+++ apps/api/i18n/messages/embed.go
@@ -2,5 +2,6 @@ package i18n_messages
 
 import "embed"
 
+// FS contains the embedded JSON message files (messages_es.json, messages_en.json).
 //go:embed messages_es.json messages_en.json
 var FS embed.FS
